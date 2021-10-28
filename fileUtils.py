3b3b532df18eb0500c5567e653b2f8fb1283d10e
helder-a-reis <helderreis78@gmail.com>
2021-10-28 12:14:18 +0200

    export

--- fileUtils.py
+++ fileUtils.py
@@ -1,4 +1,4 @@
-import json
+import json, csv
 from shutil import copyfile
 from os import path, listdir
 from os.path import isfile, getsize
@@ -17,3 +17,15 @@ def saveDictToJSON(dict, jsonFileName):
 
 def getLocaleFromFileName(fileName):
     return path.splitext(path.basename(fileName))[0]
+
+def exportToFile(sourceDict, targetDict, sourceLocale, targetLocale, onlyMissing=False):
+    targetFileName = sourceLocale + '_' + targetLocale + '.csv'
+    fieldNames = ['key', sourceLocale, targetLocale]
+    with open(targetFileName, 'w', newline='') as csvFile:
+        writer = csv.DictWriter(csvFile, fieldnames=fieldNames)
+        writer.writeheader()
+        for key, source in sourceDict.items():
+            target = targetDict.get(key, '')
+            if target != '' and onlyMissing:
+                break
+            writer.writerow({'key': key, sourceLocale: source, targetLocale: target})
